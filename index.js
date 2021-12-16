9838d33e46811486516e54fa359e40bd310ea254
Nathan Cai <nathancai1031@gmail.com>
2021-12-16 19:30:54 +0000

    added file reading

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
 const fs = require("fs");
+
 const input = "input.txt";
 
 
+const data = fs.readFileSync(input, "utf8").split("\n");
+const 
+console.log(data);
+
+
+//quick sort function
+function quickSort(array) {
+  if (array.length <= 1) {
+    return array;
+  }
+
+  const pivot = array[0];
+  
+  const left = []; 
+  const right = [];
+
+  /*
+    If the value of the element is smaller than the pivot (first element of the array) add it to the left side, else add it to the right side.
+
+    Use this process for the individual left side array that're smaller than pivot and the right side array which is greater than or equal to pivot again, rinse and repeat till there is no left and right side array or there's just one value.
+  
+   */
+  for (let i = 1; i < array.length; i++) {
+    array[i] < pivot ? left.push(array[i]) : right.push(array[i]);
+  }
+
+  console.log(pivot);
+  
+  return [...quicksort(left), ...pivot, ...quickSort(right)];
+};
