fec33d806804caa6e74b5756adef5ed956487a98
Nathan <nathancai1031@gmail.com>
2022-01-04 15:20:41 -0500

    finished writing test cases

--- __tests__/application/index.test.js
+++ __tests__/application/index.test.js
@@ -21,99 +21,6 @@ beforeAll(() => {
     if (!fs.existsSync(inputDir)) {
         fs.mkdirSync(inputDir, { recursive: true });
     }
-
-    // fs.writeFileSync(path.join(inputDir, "input1.txt"), "hello world", "utf8");
-    fs.writeFileSync(
-        path.join(inputDir, "input2a.txt"),
-        "hello\nworld",
-        "utf8"
-    );
-    fs.writeFileSync(
-        path.join(inputDir, "input2b.txt"),
-        "world\nhello",
-        "utf8"
-    );
-    fs.writeFileSync(
-        path.join(inputDir, "input3a.txt"),
-        "h\ne\nl\nl\no\nw\no\nr\nl\nd",
-        "utf8"
-    );
-    fs.writeFileSync(
-        path.join(inputDir, "input3b.txt"),
-        "i\nh\no\np\ne\nt\nh\ni\ns\nj\na\nc\nk\ns\nu\np\nm\ny\na\nv\ne\nr\na\ng\ne\nt\no\na\nm\ni\nd\nn\ni\nn\ne\nt\ny",
-        "utf8"
-    );
-    if (!fs.existsSync(outputDir)) {
-        fs.mkdirSync(outputDir, { recursive: true });
-    }
-
-    if (!fs.existsSync(inputDir)) {
-        fs.mkdirSync(inputDir, { recursive: true });
-    }
-
-    fs.writeFileSync(
-        path.join(inputDir, "input1.json"),
-        JSON.stringify(["hello world"]),
-        "utf8"
-    );
-    fs.writeFileSync(
-        path.join(inputDir, "input2a.json"),
-        JSON.stringify(["hello", "world"]),
-        "utf8"
-    );
-    fs.writeFileSync(
-        path.join(inputDir, "input2b.json"),
-        JSON.stringify(["world", "hello"]),
-        "utf8"
-    );
-    fs.writeFileSync(
-        path.join(inputDir, "input3a.json"),
-        JSON.stringify(["h", "e", "l", "l", "o", "w", "o", "r", "l", "d"]),
-        "utf8"
-    );
-    fs.writeFileSync(
-        path.join(inputDir, "input3b.json"),
-        JSON.stringify([
-            "i",
-            "h",
-            "o",
-            "p",
-            "e",
-            "t",
-            "h",
-            "i",
-            "s",
-            "j",
-            "a",
-            "c",
-            "k",
-            "s",
-            "u",
-            "p",
-            "m",
-            "y",
-            "a",
-            "v",
-            "e",
-            "r",
-            "a",
-            "g",
-            "e",
-            "t",
-            "o",
-            "a",
-            "m",
-            "i",
-            "d",
-            "n",
-            "i",
-            "n",
-            "e",
-            "t",
-            "y",
-        ]),
-        "utf8"
-    );
 });
 
 afterAll(() => {
@@ -131,7 +38,7 @@ const runProgram = async (inputFile, outputFile) => {
     return new Promise((resolve, reject) => {
         exec(`npm start ${inputFile || ""} ${outputFile || ""}`, (err) => {
             if (err) reject(err);
-            resolve();
+            else resolve();
         });
     });
 };
@@ -144,12 +51,91 @@ describe("to test the code as a CLI application as an end user", () => {
             "no such file or directory 404.txt"
         );
 
-        fs.writeFileSync(path.join(inputDir, "input1.txt"), "hello world", "utf8");
+        fs.writeFileSync(
+            path.join(inputDir, "input1.txt"),
+            "hello world",
+            "utf8"
+        );
+
+        fs.writeFileSync(
+            path.join(inputDir, "input1.js"),
+            "console.log('hello world')",
+            "utf8"
+        );
 
         await expect(
-            runProgram(path.join(inputDir, "input1.txt"), "404.txt")
-        ).rejects.toThrow("no such file or directory 404.txt");
+            runProgram(path.join(inputDir, "input1.js"))
+        ).rejects.toThrow("unsupported file type must be of type txt or json");
 
-        
+        await expect(
+            runProgram(
+                path.join(inputDir, "input1.txt"),
+                path.join(outputDir, "output.js")
+            )
+        ).rejects.toThrow("unsupported file type must be of type txt or json");
+    });
+
+    test("txt to txt", async () => {
+        fs.writeFileSync(
+            path.join(inputDir, "input2a.txt"),
+            "hello\nworld",
+            "utf8"
+        );
+
+        fs.writeFileSync(
+            path.join(inputDir, "input2b.txt"),
+            "world\nhello",
+            "utf8"
+        );
+
+        await runProgram(
+            path.join(inputDir, "input2a.txt"),
+            path.join(outputDir, "output2a.txt")
+        );
+
+        expect(
+            fs.readFileSync(path.join(outputDir, "output2a.txt"), "utf8")
+        ).toBe("hello\nworld");
+
+        await runProgram(
+            path.join(inputDir, "input2b.txt"),
+            path.join(outputDir, "output2b.txt")
+        );
+
+        expect(
+            fs.readFileSync(path.join(outputDir, "output2b.txt"), "utf8")
+        ).toBe("hello\nworld");
+    });
+
+    test("txt to json", async () => {
+        fs.writeFileSync(
+            path.join(inputDir, "input2a.txt"),
+            "hello\nworld",
+            "utf8"
+        );
+
+        fs.writeFileSync(
+            path.join(inputDir, "input2b.txt"),
+            "world\nhello",
+            "utf8"
+        );
+
+        await runProgram(
+            path.join(inputDir, "input2a.txt"),
+            path.join(outputDir, "output2a.json")
+        );
+
+        expect(
+            JSON.parse(fs.readFileSync(path.join(outputDir, "output2a.json")), "utf8")
+        ).toEqual(["hello","world"]);
+
+        await runProgram(
+            path.join(inputDir, "input2b.txt"),
+            path.join(outputDir, "output2b.json")
+        );
+
+        expect(
+            JSON.parse(fs.readFileSync(path.join(outputDir, "output2b.json")), "utf8")
+        ).toEqual(["hello","world"]);
     });
 });
--- index.js
+++ index.js
@@ -9,24 +9,18 @@ if (!inputFile) throw new Error("No input file provided");
 
 const outputFile = process.argv[3] || inputFile;
 
-const inputFileExt = path.extname(inputFile).substring(1);
+const inputFileExt = path.extname(inputFile);
 
-if(!fs.existsSync(inputFile)) throw new Error(`no such file or directory ${inputFile}`);
-else if(!fs.existsSync(outputFile)) throw new Error(`no such file or directory ${outputFile}`);
+if (!fs.existsSync(inputFile))
+    throw new Error(`no such file or directory ${inputFile}`);
 
-try {
-    switch (inputFileExt) {
-        case "txt":
-            sortTXT(inputFile, outputFile);
-            break;
-        case "json":
-            sortJSON(inputFile, outputFile);
-            break;
-        default:
-            throw new Error(
-                "incompatible file type must be of type txt or json"
-            );
-    }
-} catch (error) {
-    console.log(error);
+switch (inputFileExt) {
+    case ".txt":
+        sortTXT(inputFile, outputFile);
+        break;
+    case ".json":
+        sortJSON(inputFile, outputFile);
+        break;
+    default:
+        throw new Error("unsupported file type must be of type txt or json");
 }
